7cf7fd9dce170480b788d3699f105a651893118d
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2023-10-12 11:10:31 +0700

    (feat) : navbar pop up modal

--- components/Navbar/navbar.tsx
+++ components/Navbar/navbar.tsx
@@ -1,26 +1,43 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Style from "./index.module.css";
 import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 const Navbar = () => {
   const path = usePathname();
+  const [modal, setModal] = useState(false);
   let navTitle = "";
   if (path === "/home") {
     navTitle = "Overview";
   } else if (path === "/outlays") {
     navTitle = "Outlays Details";
   }
+  const handlePopUpModal = () => {
+    console.log("clicked");
+    setModal((prevToggle) => !prevToggle);
+  };
   return (
-    <div className={Style.navbar}>
-      <div className={Style.navbar__left}>
-        <h1>{navTitle}</h1>
+    <>
+      <div className={Style.navbar}>
+        <div className={Style.navbar__left}>
+          <h1>{navTitle}</h1>
+        </div>
+        <div className={Style.navbar__right} onClick={() => handlePopUpModal()}>
+          <div className={Style.navbar__image}></div>
+          <h1>CookyNdi</h1>
+        </div>
+        {modal && (
+          <div className={Style.navbar__right__modal}>
+            <h1>CookyNdi</h1>
+            <Link href={"/setting"}>Profile</Link>
+            <Link href={"/setting"}>Setting</Link>
+            <Link href={"/logout"}>Logout</Link>
+          </div>
+        )}
       </div>
-      <div className={Style.navbar__right}>
-        <div className={Style.navbar__image}></div>
-        <h1>CookyNdi</h1>
-      </div>
-    </div>
+      {modal && <div className={Style.navbar__right__closeModal} onClick={() => handlePopUpModal()}></div>}
+    </>
   );
 };
 
